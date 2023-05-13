3364c161b959430ec7c776547d75fae588076655
Kamlesh0007 <wwwkamleshyadav732@gmail.com>
2023-05-13 19:57:04 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [mobposition, setMobposition] = useState({ x: 0, y: 0 });
+  const isMobile = /iPhone|Android/i.test(navigator.userAgent);
+  function moveBox(direction) {
+    switch (direction) {
+      case 'up':
+        setPosition({ ...position, y: position.y - 50 });
+        break;
+      case 'down':
+        setPosition({ ...position, y: position.y + 50 });
+        break;
+      case 'left':
+        setPosition({ ...position, x: position.x - 50 });
+        break;
+      case 'right':
+        setPosition({ ...position, x: position.x + 50 });
+        break;
+      default:
+        break;
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="main_container">
+      <button className="horizontal" onClick={() => moveBox('up')} disabled={position.y === 0}>
+        Up
+      </button>
+      <div className="container">
+      <button className="vertical" onClick={() => moveBox('left')} disabled={position.x === 0}>
+        Left
+      </button>
+      <div className="main">
+          <div
+            className="box"
+            style={{ left: `${position.x}px`, top: `${position.y}px` }}
+          ></div>
+        </div>
+        <button className="vertical" onClick={() => moveBox('right')} disabled={position.x === 450}>
+          Right
+        </button>
+      </div>
+      <button className="horizontal"  onClick={() => moveBox('down')} disabled={position.y === 450}>
+        Bottom
+      </button>
+
+      </div>
+
   );
 }
 
